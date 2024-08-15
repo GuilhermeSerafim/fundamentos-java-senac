d67e2c1250363ab93826f42860ea6591d6bec00c
GUILHERME DA SILVA SERAFIM <guilherme.sserafim3@senacsp.edu.br>
2024-08-15 11:30:58 -0300

    Aula 01

---
+++ Fundamentos/src/Variaveis.java
@@ -0,0 +1,18 @@
+
+public class Variaveis {
+	public static void main(String[] args) {
+		// Primitivos
+		int x = 10;
+		double y = 1.1;
+		char letra =  'a';
+		System.out.println(letra);
+		// Classe
+		String frase = "Hello";
+		
+		letra = 98;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(frase);
+		System.out.println(letra); // Out: b -> Por causa da TABELA ASCII
+	}
+}
